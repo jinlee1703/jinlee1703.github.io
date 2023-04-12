5cc5f35cfce9176a842256b3aaba4a3a6c82e0a5
Jinwoo Lee <binarywooo@gmail.com>
2023-04-13 00:49:46 +0900

    chore: remove vendor directory

--- vendor/bundle/ruby/3.1.0/gems/sass-embedded-1.57.1-x64-mingw-ucrt/ext/sass/embedded.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Sass
-  class Embedded
-    class Compiler
-      PATH = File.absolute_path('sass_embedded/dart-sass-embedded.bat', __dir__)
-    end
-  end
-end
